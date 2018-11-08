29e1134aa65c8e2724d1a50a5750322a5663e60f
JakeWu1215 <jakewu1215@163.com>
2018-11-08 16:44:10 +0800

    [add]ideal整合测试

--- src/main/java/com/Model/ClassB.java
+++ src/main/java/com/Model/ClassB.java
@@ -3,6 +3,7 @@ package com.Model;
 public class ClassB extends ClassA {
     static {
         System.out.println("a");
+        System.out.println("bbb");
     }public ClassB(){
         System.out.println("b");
     }
